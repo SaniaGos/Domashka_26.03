094af35b0a14182bfe765472224150c346eb66a8
SaniaGos <saniagoss@gmail.com>
2021-04-01 21:54:18 +0300

    Storage

--- CommodityTurnover/Klass/Product.cs
+++ CommodityTurnover/Klass/Product.cs
@@ -7,21 +7,6 @@ namespace CommodityTurnover.Klass
 {
     class Product
     {
-        private class SortByNameHelper : IComparer
-        {
-            public int Compare(object x, object y)
-            {
-                return (x as Product).ProductName.CompareTo((y as Product).ProductName);
-            }
-        }
-
-        private class SortByPriceHelper : IComparer
-        {
-            public int Compare(object x, object y)
-            {
-                return (x as Product).Price.CompareTo((y as Product).Price);
-            }
-        }
         public int Id { get; set; }
         public string ProductName { get; set; }
         public double Price { get; set; }
@@ -32,15 +17,6 @@ namespace CommodityTurnover.Klass
         {
             return $"Product Name: {ProductName} Prise:{Price}, Id:{Id}, Category:{Category.Name}";
         }
-        public static IComparer SortByName()
-        {
-            return new SortByNameHelper();
-        }
-        public static IComparer SortByPrice()
-        {
-            return new SortByPriceHelper();
-        }
-
-
+      
     }
 }
---
+++ GameCart/Klasses/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCart.Klasses
+{
+    enum Suit
+    {
+        Heard, Diamond, Club, Spade
+    }
+    enum SizeCard
+    {
+        Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ase
+    }
+    class Card
+    {
+        public Suit Suit { get;}
+        public SizeCard SizeCard { get; private set; }
+        public Card(SizeCard sizeCard, Suit suit)
+        {
+            SizeCard = sizeCard;
+            Suit = suit;
+        }
+        public static bool operator >(Card first, Card second)
+        {
+            if (first.Suit == second.Suit)
+                if (first.SizeCard > second.SizeCard)
+                    return true;
+                else return false;
+            else throw new Exception("Suit doesn't math");
+        }
+        public static bool operator <(Card first, Card second)
+        {
+            return !(first > second);
+        }
+    }
+}
